1f2740d3a7e9244777c68b986b9eda731a854b5e
Seung Yeop Lee <2dvorak@kaist.ac.kr>
2018-02-27 22:45:44 +0900

    Updated read_csv to correctly sniff delimeters
    Sample tested containing various English names with different
    form i.e 'Seung Yeop Lee' and 'Lee, Seung Yeop' will confuse
    Python csv reader when detecting csv dialect. Previous code
    in read_csv only reads 1 line for csv.Sniffer to sniff csv
    dialect(including delimeter) which wasn't enough for
    mixed-form name csv file. So made read_csv to read sufficient bytes.

--- activity_warmup.py
+++ activity_warmup.py
@@ -6,7 +6,7 @@ import random
 def read_csv(csv_filename):
 	with open(csv_filename,'r') as csvfile:
 		# detect delimeter
-		dialect = csv.Sniffer().sniff(csvfile.readline())
+		dialect = csv.Sniffer().sniff(csvfile.read(1024))
 
 		csvfile.seek(0)
 		
